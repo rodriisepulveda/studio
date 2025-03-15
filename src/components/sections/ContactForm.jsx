0fe012954efb3f4cd0e47f6e8cf50255c292574c
Rodrigo Gabriel Sepulveda <rodrigo.sepulveda@davinci.edu.ar>
2025-03-15 05:00:02 -0300

    added images

--- src/components/sections/ContactForm.jsx
+++ src/components/sections/ContactForm.jsx
@@ -143,7 +143,7 @@ const ContactForm = () => {
                       className="w-full px-4 py-2 rounded-xl border border-gray-300 dark:border-gray-600 
                                bg-white dark:bg-gray-800 text-gray-900 dark:text-white
                                focus:ring-2 focus:ring-[#ff585e] dark:focus:ring-[#3663ff] focus:border-transparent
-                               transition-all duration-200"
+                               transition-all duration-200 required"
                     />
                   </div>
 
@@ -161,7 +161,7 @@ const ContactForm = () => {
                       className="w-full px-4 py-2 rounded-xl border border-gray-300 dark:border-gray-600 
                                bg-white dark:bg-gray-800 text-gray-900 dark:text-white
                                focus:ring-2 focus:ring-[#ff585e] dark:focus:ring-[#3663ff] focus:border-transparent
-                               transition-all duration-200"
+                               transition-all duration-200 required"
                     />
                   </div>
 
@@ -176,7 +176,7 @@ const ContactForm = () => {
                         className="w-64 px-4 py-2 rounded-xl border border-gray-300 dark:border-gray-600 
                                  bg-white dark:bg-gray-800 text-gray-900 dark:text-white
                                  focus:ring-2 focus:ring-[#ff585e] dark:focus:ring-[#3663ff] focus:border-transparent
-                                 transition-all duration-200"
+                                 transition-all duration-200 required"
                       >
                         {countries.map(country => (
                           <option key={country.code} value={country.code}>
@@ -195,7 +195,7 @@ const ContactForm = () => {
                         className="flex-1 px-4 py-2 rounded-xl border border-gray-300 dark:border-gray-600 
                                  bg-white dark:bg-gray-800 text-gray-900 dark:text-white
                                  focus:ring-2 focus:ring-[#ff585e] dark:focus:ring-[#3663ff] focus:border-transparent
-                                 transition-all duration-200"
+                                 transition-all duration-200 required"
                       />
                     </div>
                   </div>
@@ -214,7 +214,7 @@ const ContactForm = () => {
                       className="w-full px-4 py-2 rounded-xl border border-gray-300 dark:border-gray-600 
                                bg-white dark:bg-gray-800 text-gray-900 dark:text-white
                                focus:ring-2 focus:ring-[#ff585e] dark:focus:ring-[#3663ff] focus:border-transparent
-                               transition-all duration-200"
+                               transition-all duration-200 resize-none required"
                     />
                   </div>
 
